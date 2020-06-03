35a9de72447000fde907c6327b2297c66d82b0e0
Babatunde <babatundea15@gmail.com>
2020-06-03 16:03:17 +0100

    i created classes for players and room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self,name,room,age):
+        self.name=name
+        self.room=room
+        self.age=age
+        
+    def __str__(self):
+    player_details=
--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, width, colour):
+        self.name=name
+        self.width=width
+        self.colour=colour
+    def __str__(self):
+        return f"The room ${self.name} has a width of {self.width} and a colour of ${self.colour}"
+        
